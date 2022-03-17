ff45df33a9e3662fbd6eb3e7242e20003d1719a5
elmesheryakova <e.meshcheryakova@dairyculture.ru>
2022-03-17 17:35:22 +0300

    page partners/docs

--- pages/questions.vue
+++ pages/questions.vue
@@ -1,3 +1,22 @@
 <template>
-  <div></div>
+  <div class="questions">
+    <div class="container">
+      <h1 class="pages__title">Вопросы и ответы</h1>
+    </div>
+  </div>
 </template>
+<style lang="scss">
+.questions {
+  padding: 135px 0 160px;
+
+  @media (max-width: 991px) {
+    padding-top: 90px;
+  }
+  @media (max-width: 860px) {
+    padding-bottom: 60px;
+  }
+  @media (max-width: 791px) {
+    padding-top: 60px;
+  }
+}
+</style>
