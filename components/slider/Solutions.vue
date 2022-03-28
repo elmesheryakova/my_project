3b6e325cc74739d89f0c0e617d81b6f7d2e70e3a
elmesheryakova <e.meshcheryakova@dairyculture.ru>
2022-03-28 19:02:02 +0300

    sliders navigation

--- components/slider/Solutions.vue
+++ components/slider/Solutions.vue
@@ -87,6 +87,170 @@ export default {
   .swiper-button-next:after {
     opacity: 0;
   }
+  padding: 120px 0;
+  background-color: #f2f3f6;
+  position: relative;
+  height: 100vh;
+
+  .swiper-wrapper {
+    display: flex;
+    gap: 35px;
+    @media (max-width: 1024px) {
+      gap: 10px;
+    }
+  }
+
+  .swiper-slide {
+    margin-right: 80px;
+  }
+
+  .swiper-container {
+    overflow: hidden;
+  }
+
+  &__container {
+    margin-right: -600px;
+  }
+
+  .swiper-button-prev,
+  .swiper-button-next {
+    width: 45px;
+    height: 45px;
+    position: absolute;
+    top: 50%;
+    transform: translateY(-50%);
+  }
+
+  .swiper-button-prev {
+    transform: rotate(180deg);
+    left: 0;
+    display: none;
+  }
+
+  .swiper-button-next {
+    right: 0;
+
+    &::before {
+      position: absolute;
+      content: "";
+      top: -250px;
+      right: 0;
+      height: 600px;
+      width: 300px;
+      background: linear-gradient(
+          -90deg,
+          #f2f3f7 0%,
+          rgba(242, 243, 247, 0) 100%
+        ),
+        linear-gradient(-90deg, #f2f3f7 0%, rgba(242, 243, 247, 0) 100%);
+
+      z-index: -1;
+      @media (max-width: 1940px) {
+        top: -210px;
+      }
+      @media (max-width: 1024px) {
+        width: 150px;
+      }
+    }
+    @media (max-width: 1940px) {
+      top: 60%;
+    }
+  }
+
+  &__title {
+    padding: 20px 20px 0;
+    @media (max-width: 1024px) {
+      padding: 10px 10px 0;
+    }
+  }
+
+  &__item {
+    background-color: #fff;
+    margin-bottom: 60px;
+    position: relative;
+
+    &::after {
+      position: absolute;
+      content: "";
+      width: 100%;
+      bottom: -28px;
+      left: 0;
+      z-index: 8;
+      border: 30px solid transparent;
+      border-top: 0px solid transparent;
+      border-left: 25px solid #fff;
+    }
+  }
+
+  &__img {
+    padding: 20px;
+
+    img {
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+    }
+
+    // @media (max-width: 1024px) {
+    //   width: 200px;
+    //   height: 180px;
+    //   padding: 10px;
+    // }
+  }
+
+  &__icon {
+    width: 160px;
+    height: 160px;
+    background-color: #fff;
+    cursor: pointer;
+    position: relative;
+
+    .svg-color,
+    .svg-white {
+      position: absolute;
+      width: 100%;
+      height: 100%;
+      fill: transparent;
+      padding: 20px;
+    }
+
+    .svg-white {
+      opacity: 0;
+    }
+
+    @media (max-width: 1024px) {
+      width: 120px;
+      height: 120px;
+    }
+  }
+
+  &__wrap {
+    cursor: pointer;
+
+    &:hover {
+      .svg-color {
+        opacity: 0;
+        transition: 0.3s ease-out;
+      }
+
+      .svg-white {
+        opacity: 1;
+        background-color: $primary;
+        transition: 0.3s ease-out;
+      }
+
+      .slider-sol__item {
+        box-shadow: 0px 20px 60px 0px #17202929;
+        transition: 0.3s ease-out;
+      }
+    }
+  }
+
+  &--frontpage {
+    min-height: 100vh;
+    padding-bottom: 160px;
+    height: auto;
+  }
 }
 </style>
 
