d2457868ab2c5dd8483ce57417a64f4716ba43c2
Bogdan Dolin <d.bogdan@inbox.ru>
2022-04-27 18:49:48 +0700

    Fix bottle pin bug on resize

--- pages/index.vue
+++ pages/index.vue
@@ -731,7 +731,7 @@ export default {
           trigger: "#anim-" + bottleType + "-container",
           spacer: false,
           pinSpacing: false,
-          pinType: "fixed",
+          pinType: "transform",
           pinnedContainer: null,
           start: "top",
           endTrigger: endTriggerElem,
